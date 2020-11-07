7f59a5a8101da32aceb6858c5d1d8478304fa405
exuanbo <exuanbo@protonmail.com>
2020-11-07 00:51:47 +0000

    refactor: rm try catch block in `run()`
    refactor: rename variable
    
    refactor: use or inplace of if

--- src/run.ts
+++ src/run.ts
@@ -5,38 +5,30 @@ import * as github from '@actions/github'
 import { showInputs, getInputs } from './inputs'
 
 export const run = async (): Promise<void> => {
-  try {
-    const inp = getInputs()
-    showInputs(inp)
+  const inp = getInputs()
+  showInputs(inp)
 
-    core.startGroup('Read file content')
-    const workSpace = process.env.GITHUB_WORKSPACE as string
-    const filePath = path.join(workSpace, inp.FilePath)
-    const fileContent = fs.readFileSync(filePath, 'utf-8')
-    core.info(`[INFO] Done with file "${filePath}"`)
-    core.endGroup()
+  core.startGroup('Read file content')
+  const workSpace = process.env.GITHUB_WORKSPACE as string
+  const filepath = path.join(workSpace, inp.FilePath)
+  const content = fs.readFileSync(filepath, 'utf-8')
+  core.info(`[INFO] Done with file "${filepath}"`)
+  core.endGroup()
 
-    core.startGroup('Deploy to gist')
-    const octokit = github.getOctokit(inp.Token)
-    const fileName =
-      inp.GistFileName !== undefined
-        ? inp.GistFileName
-        : path.basename(filePath)
-    await octokit.gists.update({
-      gist_id: inp.GistID,
-      files: {
-        [fileName]: {
-          filename: fileName,
-          content: fileContent
-        }
+  core.startGroup('Deploy to gist')
+  const octokit = github.getOctokit(inp.Token)
+  const filename = inp.GistFileName || path.basename(filepath)
+  await octokit.gists.update({
+    gist_id: inp.GistID,
+    files: {
+      [filename]: {
+        filename,
+        content
       }
-    })
-    core.info(`[INFO] Done with gist "${inp.GistID}/${fileName}"`)
-    core.endGroup()
+    }
+  })
+  core.info(`[INFO] Done with gist "${inp.GistID}/${filename}"`)
+  core.endGroup()
 
-    core.info('[INFO] Action successfully completed')
-  } catch (err) {
-    if (err instanceof Error) throw new Error(err.message)
-    else throw err
-  }
+  core.info('[INFO] Action successfully completed')
 }
